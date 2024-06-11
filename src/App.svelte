4cd9e61cc6d1e344e3ad96dca4641995247c102e
kels7 <hiketabo@gmail.com>
2024-06-11 12:01:58 +0000

    feat: add getting updated balance

--- src/App.svelte
+++ src/App.svelte
@@ -3,19 +3,28 @@
   import { XianWalletUtils } from "./xian-dapp-utils";
   
   let walletInfo;
-  onMount(()=>{
+  let balance;
+
+  onMount(async()=>{
       XianWalletUtils.init();
+      balance = await XianWalletUtils.getBalance("currency");
   });
   
   const requestWalletInfo = async()=> {
       walletInfo = await XianWalletUtils.requestWalletInfo();
   }
 
+  const updateBalance = async() => {
+     balance = await XianWalletUtils.getBalance("currency");
+  }
+
 </script>
 
 <main>
   <button on:click={requestWalletInfo}>getWalletInfo</button>
+  <button on:click={updateBalance}>getCurrentBalance</button>
   <p>address: {walletInfo?.address?walletInfo.address:"0x"}</p>
+  <p>balance: {balance?balance: 0}</p>
   <p>chainId: {walletInfo?.chainId?walletInfo.chainId:"<chain-id>"}</p>
   <p>locked: {walletInfo?.locked}</p>
 </main>
